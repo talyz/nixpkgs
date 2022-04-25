e86cb81e4e16f40381ef1674b00efcb21fc1411e
talyz <kim.lindberger@gmail.com>
2022-04-25 12:40:30 +0200

    nixos/nomachine: format and refactor for readability

--- nixos/modules/services/admin/nomachine.nix
+++ nixos/modules/services/admin/nomachine.nix
@@ -1,47 +1,40 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
   cfg = config.services.nxserver;
 
   settingsFmt = rec {
     type = with lib.types;
-      (attrsOf (nullOr (oneOf [str int bool attrs])))
-      // {
-        description = "settings option";
-      };
+      attrsOf (nullOr (oneOf [ str int bool attrs ]));
 
     format = value:
-      lib.generators.toKeyValue {
-        mkKeyValue = k: v:
-          if lib.isAttrs v
-          then "Section \"${k}\"\n" + (format v) + "EndSection"
-          else if lib.isBool v
-          then
-            lib.generators.mkKeyValueDefault {} " " k (
-              if v == true
-              then 1
-              else 0
-            )
-          else if lib.isString v
-          then
-            lib.generators.mkKeyValueDefault {} " " k (
-              if k == "Name"
-              then "\"${v}\""
-              else v
-            )
-          else lib.generators.mkKeyValueDefault {} " " k v;
-      }
-      value;
+      lib.generators.toKeyValue
+        {
+          mkKeyValue = k: v:
+            let
+              default = lib.generators.mkKeyValueDefault { } " " k;
+            in
+            if lib.isAttrs v then
+              "Section \"${k}\"\n" + (format v) + "EndSection"
+            else if lib.isBool v then
+              default (if v == true then 1 else 0)
+            else if lib.isString v then
+              default (if k == "Name" then ''"${v}"'' else v)
+            else
+              default v;
+        }
+        value;
 
     generate = name: value:
       pkgs.writeText name (
         format value
       );
   };
-in {
+in
+{
   options.services.nxserver = with lib; {
     enable = mkEnableOption "the NoMachine remote desktop server";
 
@@ -67,7 +60,7 @@ in {
         Settings for the NoMachine nxserver instance.
       '';
 
-      default = {};
+      default = { };
       example = {
         EnableDebug = true;
         SessionLogLevel = 6;
@@ -91,7 +84,7 @@ in {
         Settings for the NoMachine nxnode instance.
       '';
 
-      default = {};
+      default = { };
       example = {
         EnableDebug = true;
         SessionLogLevel = 6;
@@ -122,7 +115,7 @@ in {
     };
 
     users.groups = {
-      "nx" = {};
+      "nx" = { };
     };
 
     services.nxserver = {
@@ -216,14 +209,14 @@ in {
       };
     };
 
-    networking.firewall.allowedTCPPorts = lib.mkIf (cfg.openFirewall) [cfg.serverSettings.Server.Port];
+    networking.firewall.allowedTCPPorts = lib.mkIf (cfg.openFirewall) [ cfg.serverSettings.Server.Port ];
 
     systemd.services.nxserver = {
       description = "NoMachine Server daemon";
-      wantedBy = ["multi-user.target"];
-      after = ["syslog.target" "network.target" "network-online.target" "display-manager.service"];
-      wants = ["network-online.target"];
-      bindsTo = ["display-manager.service"];
+      wantedBy = [ "multi-user.target" ];
+      after = [ "syslog.target" "network.target" "network-online.target" "display-manager.service" ];
+      wants = [ "network-online.target" ];
+      bindsTo = [ "display-manager.service" ];
 
       serviceConfig = {
         User = "nx";
@@ -245,67 +238,69 @@ in {
           "nxserver/scripts"
         ];
 
-        ExecStartPre = let
-          preStartScript = ''
-            set -euo pipefail
+        ExecStartPre =
+          let
+            preStartScript = ''
+              set -euo pipefail
 
-            # always update to current version of files
-            cp ${cfg.package}/NX/etc.static/version /etc/NX/
-            cp ${cfg.package}/NX/etc.static/update.cfg /etc/NX/
+              # always update to current version of files
+              cp ${cfg.package}/NX/etc.static/version /etc/NX/
+              cp ${cfg.package}/NX/etc.static/update.cfg /etc/NX/
 
-            if [ ! -f /etc/NX/usb.db ]; then
-              cp ${cfg.package}/NX/etc.static/usb.db /etc/NX/
-            fi
-
-            # nx processes run as user "nx" need to write state and lock files to /etc/NX
-            chown nx:nx /etc/NX
-
-            # Almost all shell scripts in scripts/restriced need u+s and to be owned by root
-            # Only symlinking the restricted folder does not work because of checks done by
-            # nxexec (symlink is not a folder)
-            cp -r ${cfg.package}/NX/scripts.static/* /run/nxserver/scripts/
-            for i in /run/nxserver/scripts/restricted/*.sh; do
-              file=$(basename "$i")
-              if [ "$file" != "nxfunct.sh" ] && [ "$file" != "nxlogrotate.sh" ]; then
-                chown root:root "$i"
-                chmod u+s "$i"
+              if [ ! -f /etc/NX/usb.db ]; then
+                cp ${cfg.package}/NX/etc.static/usb.db /etc/NX/
               fi
-            done
 
-            # nxserver can only run after the previous commands
+              # nx processes run as user "nx" need to write state and lock files to /etc/NX
+              chown nx:nx /etc/NX
 
-            if [ ! -f /etc/NX/uuid ]; then
-              ${cfg.package}/NX/bin/nxkeygen -u > /etc/NX/uuid
-            fi
+              # Almost all shell scripts in scripts/restriced need u+s and to be owned by root
+              # Only symlinking the restricted folder does not work because of checks done by
+              # nxexec (symlink is not a folder)
+              cp -r ${cfg.package}/NX/scripts.static/* /run/nxserver/scripts/
+              for i in /run/nxserver/scripts/restricted/*.sh; do
+                file=$(basename "$i")
+                if [ "$file" != "nxfunct.sh" ] && [ "$file" != "nxlogrotate.sh" ]; then
+                  chown root:root "$i"
+                  chmod u+s "$i"
+                fi
+              done
 
-            if [ ! -f /etc/NX/server.lic ]; then
-              cp ${cfg.package}/NX/etc.static/server.lic.sample /etc/NX/
-              /etc/NX/nxserver --validate
-              mv /etc/NX/server.lic.sample /etc/NX/server.lic
-              chown nx:root /etc/NX/server.lic
-              chmod 0400 /etc/NX/server.lic
-            fi
+              # nxserver can only run after the previous commands
 
-            if [ ! -f /etc/NX/node.lic ]; then
-              cp ${cfg.package}/NX/etc.static/node.lic.sample /etc/NX/
-              /etc/NX/nxnode --validate
-              mv /etc/NX/node.lic.sample /etc/NX/node.lic
-              chown nx:root /etc/NX/node.lic
-              chmod 0400 /etc/NX/node.lic
-              /etc/NX/nxserver --validatenode
-            fi
+              if [ ! -f /etc/NX/uuid ]; then
+                ${cfg.package}/NX/bin/nxkeygen -u > /etc/NX/uuid
+              fi
 
-            # This inits the database with the localhost_4000 display entry
-            /etc/NX/nxserver --addtoredis
+              if [ ! -f /etc/NX/server.lic ]; then
+                cp ${cfg.package}/NX/etc.static/server.lic.sample /etc/NX/
+                /etc/NX/nxserver --validate
+                mv /etc/NX/server.lic.sample /etc/NX/server.lic
+                chown nx:root /etc/NX/server.lic
+                chmod 0400 /etc/NX/server.lic
+              fi
 
-            if [ ! -f /etc/NX/keys/host/nx_host_rsa_key ]; then
-              mkdir -p /etc/NX/keys/host
-              ${cfg.package}/NX/bin/nxkeygen -k /etc/NX/keys/host/nx_host_rsa_key -c /etc/NX/keys/host/nx_host_rsa_key.crt
-              chown -R nx:root /etc/NX/keys
-              chmod 0400 /etc/NX/keys/host/nx_host_rsa_key
-            fi
-          '';
-        in "+${pkgs.writeShellScript "nxserver-prestart" preStartScript}";
+              if [ ! -f /etc/NX/node.lic ]; then
+                cp ${cfg.package}/NX/etc.static/node.lic.sample /etc/NX/
+                /etc/NX/nxnode --validate
+                mv /etc/NX/node.lic.sample /etc/NX/node.lic
+                chown nx:root /etc/NX/node.lic
+                chmod 0400 /etc/NX/node.lic
+                /etc/NX/nxserver --validatenode
+              fi
+
+              # This inits the database with the localhost_4000 display entry
+              /etc/NX/nxserver --addtoredis
+
+              if [ ! -f /etc/NX/keys/host/nx_host_rsa_key ]; then
+                mkdir -p /etc/NX/keys/host
+                ${cfg.package}/NX/bin/nxkeygen -k /etc/NX/keys/host/nx_host_rsa_key -c /etc/NX/keys/host/nx_host_rsa_key.crt
+                chown -R nx:root /etc/NX/keys
+                chmod 0400 /etc/NX/keys/host/nx_host_rsa_key
+              fi
+            '';
+          in
+          "+${pkgs.writeShellScript "nxserver-prestart" preStartScript}";
         ExecStart = "${cfg.package}/bin/nxserver --daemon";
         KillMode = "process";
         SuccessExitStatus = "0 SIGTERM";
